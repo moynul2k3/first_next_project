7eef3e6de730c0ff930a61d6e41ecaeb8d4b12a7
moynul2k3 <moynul.officials@gmail.com>
2025-02-15 00:29:45 +0600

    hola

--- src/components/registration/register.tsx
+++ src/components/registration/register.tsx
@@ -6,7 +6,7 @@ type RegisterProps = {
 
 const Register: React.FC<RegisterProps> = ({children}) => {
     return (
-        <div className='max-md:h-[500px] md:h-[480px] max-md:w-full md:w-[1000px] md:flex md:justify-between    '>
+        <div className='max-md:h-[600px] md:h-[480px] max-md:w-full md:w-[1000px] md:flex md:justify-between    '>
             <div className='p-5 flex-1 flex justify-center items-center max-md:hidden'>
                 <Image
                     className=""
