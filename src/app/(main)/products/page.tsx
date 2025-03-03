3c26f7de2fd76f45d9e76a3e98dfd288d71a08d3
moynul2k3 <moynul.officials@gmail.com>
2025-03-04 04:06:25 +0600

    edited

--- src/app/(main)/products/page.tsx
+++ src/app/(main)/products/page.tsx
@@ -99,7 +99,7 @@ const ProductsPage = () => {
           }
 
       </main>
-      <div className="absolute md:hidden px-4 bottom-24 right-0 w-full z-50 bg-white pt-2 pb-5 border-t-[1px] border-black/10">
+      <div className="absolute md:hidden px-4 bottom-28 right-0 w-full z-50 bg-white pt-2 pb-5 border-t-[1px] border-black/10">
         <BottomNav  />
       </div>
     </div>
